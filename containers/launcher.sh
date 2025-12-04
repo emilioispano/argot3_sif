06299822489ce20ba4abfdfcee7345a6995903b2
emilioispano <emilio.ispano@phd.unipd.it>
2025-12-04 15:21:01 +0100

    bug fix

--- containers/launcher.sh
+++ containers/launcher.sh
@@ -1,47 +1,86 @@
-fasta=$1
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# -----------------------------
+# Usage function
+# -----------------------------
+usage() {
+    echo "Usage: $0 [options]"
+    echo
+    echo "Options:"
+    echo "  -f <fasta>        Input FASTA file for DIAMOND/Argot"
+    echo "  -o <outdir>       Output directory for results"
+    echo "  -h                Show this help message"
+    echo
+    echo "Example:"
+    echo "  $0 -f /path/to/targets.fasta -o /path/to/results/"
+    exit 1
+}
+
+# -----------------------------
+# Parse command-line options
+# -----------------------------
+
+fasta=""
+results=""
+
+while getopts ":f:o:h" opt; do
+    case $opt in
+        f) fasta=$OPTARG ;;
+        o) results=$OPTARG ;;
+        h) usage ;;
+        \?) echo "Invalid option: -$OPTARG" >&2; usage ;;
+        :) echo "Option -$OPTARG requires an argument." >&2; usage ;;
+    esac
+done
+
+# -----------------------------
+# Check required arguments
+# -----------------------------
+if [[ -z "$fasta" || -z "$results"  ]]; then
+    echo "Error: -f <fasta>, and -o <outdir> are required arguments"
+    exit 1
+fi
+
+src=/argot3/src
+data=$results/data
+preds=$results/predictions
+mkdir -p $results $data $preds
 
 echo "CHECKING FASTA HEADER FORMAT..."
-mkdir -p /wdir/data
-python3 /argot3/src/check_fasta.py -f $fasta -o /wdir/data/proteins_list.fasta
+python3 $src/check_fasta.py -f $fasta -o $data/proteins_list.fasta
 
 echo "EXTRACTING PROTEINS LISTS"
-grep ">" /wdir/data/proteins_list.fasta |cut -d" " -f1 |sort --parallel=32 |uniq |sed 's/>//g' > /wdir/data/proteins_list.txt
+grep ">" $data/proteins_list.fasta |cut -d" " -f1 |sort --parallel=32 |uniq |sed 's/>//g' > $data/proteins_list.txt
 
 echo "COMPUTING EMBEDDINGS FIRST"
-python3 /argot3/src/extract.py esm2_t33_650M_UR50D /wdir/data/proteins_list.fasta /wdir/data/torch_embeddings --repr_layers 33 --include per_tok
+python3 $src/extract.py esm2_t33_650M_UR50D $data/proteins_list.fasta $data/torch_embeddings --repr_layers 33 --include per_tok
 
 echo "EXTRACTING FAILED PROTEINS (if any)"
-ls -1 /wdir/data/torch_embeddings | cut -d"." -f1 | sort | uniq > /wdir/data/embedded_prots.txt
+ls -1 $data/torch_embeddings | cut -d"." -f1 | sort | uniq > $data/embedded_prots.txt
 
-n_total=$(wc -l < /wdir/data/proteins_list.txt)
-n_embedded=$(wc -l < /wdir/data/embedded_prots.txt)
+n_total=$(wc -l < $data/proteins_list.txt)
+n_embedded=$(wc -l < $data/embedded_prots.txt)
 
 if [ "$n_total" -ne "$n_embedded" ]; then
     echo "WARNING: Some proteins were not embedded. Attempting re-embedding..."
-    fail_uids=/wdir/data/without_embeddings.txt
-    fail_fasta=/wdir/data/without_embeddings.fasta
-    comm -13 /wdir/data/embedded_prots.txt /wdir/data/proteins_list.txt > /wdir/data/proteins_list.fasta
-    python3 /argot3/src/get_fastas_uniprot.py -i /wdir/data/proteins_list.fasta -u $fasta -o /wdir/data/without_embeddings.fasta
-    python3 /argot3/src/check_fasta.py -f /wdir/data/without_embeddings.fasta -o /wdir/data/without_embeddings_clean.fasta
-    python3 /argot3/extract.py esm2_t33_650M_UR50D /wdir/data/without_embeddings_clean.fasta /wdir/data/torch_embeddings --repr_layers 33 --include per_tok --nogpu
+    fail_uids=$data/without_embeddings.txt
+    fail_fasta=$data/without_embeddings.fasta
+    comm -13 $data/embedded_prots.txt $data/proteins_list.txt > $data/without_embeddings.txt
+    python3 $src/get_fastas_uniprot.py -i $data/without_embeddings.txt -u $data/proteins_list.fasta -o $data/without_embeddings.fasta
+    python3 $src/check_fasta.py -f $without_embeddings.fasta -o $data/without_embeddings_clean.fasta
+    python3 $src/extract.py esm2_t33_650M_UR50D $data/without_embeddings_clean.fasta $data/torch_embeddings --repr_layers 33 --include per_tok --nogpu
 fi
 
 echo "CONVERTING"
-python3 /argot3/src/convert_to_tf.py -e /wdir/data/torch_embeddings -o /wdir/data/embeddings
-rm -rf /wdir/data/torch_embeddings
+python3 $src/convert_to_tf.py -e $data/torch_embeddings -o $data/embeddings
+rm -rf $data/torch_embeddings
 
 #echo "COMPUTING PREDICTIONS"
-pred_ont=/wdir/data/prediction_by_ontology
-pred_all=/wdir/output
 
-mkdir -p $pred_ont
-mkdir -p $pred_all
-
-python3 /argot3/src/predict_batch.py -l /wdir/data/proteins_list.txt -e /wdir/data/embeddings -o $pred_ont -b 16 -s /argot3/input/structure/ -w /argot3/input/weights/
-python3 /argot3/src/join.py -c $pred_ont/cco_batch.txt -m $pred_ont/mfo_batch.txt -b $pred_ont/bpo_batch.txt -o $pred_all/prediction_raw.txt
-python3 /argot3/src/propagate.py -i $pred_all/prediction_raw.txt -o $pred_all/prediction_propagated.txt -g /argot3/input/go/go.owl
-python3 /argot3/src/format_out.py -i $pred_all/prediction_raw.txt -g /argot3/input/go/go.owl -o $pred_all/prediction_raw.txt.final
-python3 /argot3/src/format_out.py -i $pred_all/prediction_propagated.txt -g /argot3/input/go/go.owl -o $pred_all/prediction_propagated.txt.final
-mv $pred_all/prediction_raw.txt.final $pred_all/predictions_raw.tsv
-mv $pred_all/prediction_propagated.txt.final $pred_all/predictions_prop.tsv
-rm $pred_all/prediction_raw.txt $pred_all/prediction_propagated.txt
+python3 $src/predict_batch.py -l $data/proteins_list.txt -e $data/embeddings -o $data -b 16 -s /argot3/input/structure/ -w /argot3/input/weights/
+python3 $src/join.py -c $data/cco_batch.txt -m $data/mfo_batch.txt -b $data/bpo_batch.txt -o $data/prediction_raw.txt
+python3 $src/propagate.py -i $data/prediction_raw.txt -o $data/prediction_propagated.txt -g /argot3/input/go/go.owl
+python3 $src/format_out.py -i $data/prediction_raw.txt -g /argot3/input/go/go.owl -o $preds/unpropagated.tsv
+python3 $src/format_out.py -i $data/prediction_propagated.txt -g /argot3/input/go/go.owl -o $preds/propagated.tsv
